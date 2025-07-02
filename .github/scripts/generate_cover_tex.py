e6d8e84e4fa6bef5445a7b3bf8518e57acbab937
kazu-321 <kzs321kzs@gmail.com>
2025-07-02 11:14:21 +0900

    fix some

--- .github/scripts/generate_cover_tex.py
+++ .github/scripts/generate_cover_tex.py
@@ -22,32 +22,32 @@ def generate_cover_tex(settings_path, output_path):
     final_accept = latex_escape(config.get("final_acceptance_date", ""))
 
     # 実験日をparbox用に整形
-    ex_dates_lines = "\\\\ \n".join([f"& \\underline{{{latex_escape(d)}}} \\\[0.5cm]" for d in ex_dates])
+    ex_dates_lines = "\\\\ \n".join([f"& \\underline{{{latex_escape(d)}}}[0.5cm]" for d in ex_dates])
 
     lines = [
         "\\begin{titlepage}",
         "\\centering",
         "\\vspace*{2cm}",
         "",
-        f"{{\\Huge\\bfseries {title}}}\\\\[2cm]",
+        f"{{\\Huge\\bfseries {title}}}[2cm]",
         "\\begin{tabular}{@{}l l}",
         "  \\rule{0pt}{2ex}\\textbf{実験題目} & \\\\",
         f"                                    & \\underline{{{subject}}} \\\\",
-        "\\end{tabular}\\\\[3cm]",
+        "\\end{tabular}[3cm]",
         "",
         "\\begin{flushleft}",
         "\\begin{tabular}{@{}l l}",
         f"  指導教員 & \\\\",
-        f"           & \\underline{{{teacher}}} \\\[0.5cm]",
+        f"           & \\underline{{{teacher}}}[0.5cm]",
         "",
         f"  実験日 & \\\\"
         f"         {ex_dates_lines}",
         "レポート \\\\",
-        f"  提出締切日 & \\underline{{{deadline}}} \\\[0.5cm]",
+        f"  提出締切日 & \\underline{{{deadline}}}[0.5cm]",
         "レポート\\\\",
-        f"  提出受理最終日 & \\underline{{{final_accept}}} \\\[0.5cm]",
+        f"  提出受理最終日 & \\underline{{{final_accept}}}[0.5cm]",
         "レポート\\\\",
-        f"  提出日 & \\underline{{{submit_date}}} \\\[0.5cm]",
+        f"  提出日 & \\underline{{{submit_date}}}[0.5cm]",
         "",
         f"  報告者 & \\underline{{{author}}} ",
         "\\end{tabular}",
